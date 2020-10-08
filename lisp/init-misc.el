fa1d3f268ad9ee9ae9bc56ae144fdc005c486f0c
Pastnontra <Pastnontra@gmail.com>
2020-10-08 13:13:12 +0800

    first commit

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -1204,7 +1204,10 @@ See https://github.com/RafayGhafoor/Subscene-Subtitle-Grabber."
 (setq visible-bell t)
 
 (add-to-list 'default-frame-alist
-             '(font . "Hack-10"))
+             '(font . "Hack-12"))
+(dolist (charset '(kana han cjk-misc bopomofo))
+  (set-fontset-font (frame-parameter nil 'font) charset
+                    (font-spec :family "微软雅黑" :size 15)))
 
 (global-set-key (kbd "C-h") 'delete-backward-char)
 (define-key isearch-mode-map "\C-h" 'isearch-delete-char)
