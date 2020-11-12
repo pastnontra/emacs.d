6ca0a5061906367ec35df52ee843afe0c71070ac
Pastnontra <pastnontra@gmail.com>
2020-11-12 12:47:24 +0800

    linux commit

--- lisp/init-config.el
+++ lisp/init-config.el
@@ -2,20 +2,23 @@
 
 (setq visible-bell t)
 
-;; set font
-(add-to-list 'default-frame-alist
-             '(font . "Hack-12"))
-(dolist (charset '(kana han cjk-misc bopomofo))
-  (set-fontset-font (frame-parameter nil 'font) charset
-                    (font-spec :family "微软雅黑")))
+;; font
+(set-face-attribute 'default nil :font "hack 12")
+(setq face-font-rescale-alist '(("等距更纱黑体 T SC" . 1)))
+
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+  (set-fontset-font (frame-parameter nil 'font)
+                    charset
+                        (font-spec :family "等距更纱黑体 T SC")))
+
 
 (global-set-key (kbd "C-h") 'delete-backward-char)
 (define-key isearch-mode-map "\C-h" 'isearch-delete-char)
 
 (require 'init-evil)
 (require 'evil-matchit)
-(setq my-use-m-for-matchit t)
 
+;; theme
 (load-theme 'doom-monokai-pro t)
 
 ;; In light environment, there are little different between above 3 color
