597d1deab49201104c2912ff864f561835df6daa
Pastnontra <pastnontra@gmail.com>
2021-05-20 18:24:06 +0800

    test awesome-pair

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -10,4 +10,10 @@
    :url "https://gitclone.com/github.com/manateelazycat/emacs-application-framework.git"
    :files ("*")))
 
+(quelpa
+ '(awesome-pair
+   :fetcher git
+   :url "https://github.com/manateelazycat/awesome-pair"
+   :files ("*")))
+
 (provide 'init-package)
