4474622e13a2e550d12e7613f7864eeb5b024dca
Pastnontra <pastnontra@gmail.com>
2021-06-03 11:47:12 +0800

    update

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -4,25 +4,50 @@
   ;; optimization, no need to activate all the packages so early
   (cond
    (*emacs27*
+    ;; "package-quickstart.el" converts path in `load-path' into
+    ;; os dependent path, make it impossible to share same emacs.d between
+    ;; Windows and Cygwin.
+    (unless (or *win64* *cygwin*)
     ;; you need run `M-x package-quickstart-refresh' at least once
     ;; to generate file "package-quickstart.el'.
     ;; It contains the `autoload' statements for all packages.
     ;; Please note once this file is created, you can't automatically
     ;; install missing package any more
     ;; You also need need re-generate this file if any package is upgraded.
-    (setq package-quickstart t)
+    (setq package-quickstart t))
 
     ;; esup need call `package-initialize'
     ;; @see https://github.com/jschaf/esup/issues/84
     (when (or (featurep 'esup-child)
               (fboundp 'profile-dotemacs)
-              (not (file-exists-p (concat my-emacs-d "elpa")))
+              ;; (not (file-exists-p (concat my-emacs-d "elpa")))
               (my-vc-merge-p)
 	      noninteractive)
      (package-initialize)))
  (t
   ;; @see https://www.gnu.org/software/emacs/news/NEWS.27.1
-    (package-initialize))))
+  ;; ** Installed packages are now activated *before* loading the init file.
+  ;; As a result of this change, it is no longer necessary to call
+  ;; 'package-initialize' in your init file.
+
+  ;; Previously, a call to 'package-initialize' was automatically inserted
+  ;; into the init file when Emacs was started.  This call can now safely
+  ;; be removed.  Alternatively, if you want to ensure that your init file
+  ;; is still compatible with earlier versions of Emacs, change it to:
+
+  ;; (when (< emacs-major-version 27)
+  ;;   (package-initialize))
+
+  ;; However, if your init file changes the values of 'package-load-list'
+  ;; or 'package-user-dir', or sets 'package-enable-at-startup' to nil then
+  ;; it won't work right without some adjustment:
+  ;; - You can move that code to the early init file (see above), so those
+  ;;   settings apply before Emacs tries to activate the packages.
+  ;; - You can use the new 'package-quickstart' so activation of packages
+  ;;   does not need to pay attention to 'package-load-list' or
+  ;;   'package-user-dir' any more.
+  ;;(package-initialize)
+  )))
 
 (my-initialize-package)
 
@@ -140,6 +165,7 @@
     moonscript
     all-the-icons
     doom-modeline
+    org-journal
     ;; depended by chemacs2
     kv)
   "Packages to install from melpa-unstable.")
@@ -165,7 +191,7 @@
 
         ;; {{ Option 2: tsinghua mirror repository
         ;; ;; @see https://mirror.tuna.tsinghua.edu.cn/help/elpa/ on usage:
-        ;; ("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+        ("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
         ;; ("melpa-stable" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa-stable/")
         ;; }}
@@ -371,6 +397,7 @@ You still need modify `package-archives' in \"init-elpa.el\" to PERMANENTLY use 
 ;; my installed
 (require-package 'rime)
 (require-package 'org-superstar)
+(require-package 'org-journal)
 (require-package 'ctable)
 (require-package 'deferred)
 (require-package 'epc)
