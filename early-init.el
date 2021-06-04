cdf382cb9cbf54360d82fd383db6a229ca98444d
Pastnontra <pastnontra@gmail.com>
2021-06-04 23:13:29 +0800

    upstream:fixed emacs 26 ci issue

--- early-init.el
+++ early-init.el
@@ -1,3 +1,10 @@
+(when (or (featurep 'esup-child)
+          (fboundp 'profile-dotemacs)
+          (daemonp)
+          (boundp 'startup-now)
+          noninteractive)
+  (setq package-enable-at-startup nil))
+
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
