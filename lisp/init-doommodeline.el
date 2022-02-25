fe939d7c373999317d667b77d55b7c68ca8ba97a
Pastnontra <pastnontra@gmail.com>
2022-02-25 23:14:29 +0800

    Fix CI

--- lisp/init-doommodeline.el
+++ lisp/init-doommodeline.el
@@ -42,51 +42,51 @@
 ; ;;   truncate-upto-root => ~/P/F/e/lisp/comint.el
 ; ;;   truncate-all => ~/P/F/e/l/comint.el
 ; ;;   truncate-nil => ~/Projects/FOSS/emacs/lisp/comint.el
-; ;;   relative-from-project => emacs/lisp/comint.el
-; ;;   relative-to-project => lisp/comint.el
-; ;;   file-name => comint.el
-; ;;   buffer-name => comint.el<2> (uniquify buffer name)
-; ;;
-; ;; If you are experiencing the laggy issue, especially while editing remote files
-; ;; with tramp, please try `file-name' style.
-; ;; Please refer to https://github.com/bbatsov/projectile/issues/657.
-; (setq doom-modeline-buffer-file-name-style 'auto)
-;
-; ;; Whether display icons in the mode-line.
-; ;; While using the server mode in GUI, should set the value explicitly.
-; (setq doom-modeline-icon (display-graphic-p))
-;
-; ;; Whether display the icon for `major-mode'. It respects `doom-modeline-icon'.
-; (setq doom-modeline-major-mode-icon t)
-;
-; ;; Whether display the colorful icon for `major-mode'.
-; ;; It respects `all-the-icons-color-icons'.
-; (setq doom-modeline-major-mode-color-icon t)
-;
-; ;; Whether display the icon for the buffer state. It respects `doom-modeline-icon'.
-; (setq doom-modeline-buffer-state-icon t)
-;
-; ;; Whether display the modification icon for the buffer.
-; ;; It respects `doom-modeline-icon' and `doom-modeline-buffer-state-icon'.
-; (setq doom-modeline-buffer-modification-icon t)
-;
-; ;; Whether to use unicode as a fallback (instead of ASCII) when not using icons.
-; (setq doom-modeline-unicode-fallback nil)
-;
-; ;; Whether display the minor modes in the mode-line.
-; (setq doom-modeline-minor-modes nil)
-;
-; ;; If non-nil, a word count will be added to the selection-info modeline segment.
-; (setq doom-modeline-enable-word-count nil)
-;
-; ;; Major modes in which to display word count continuously.
-; ;; Also applies to any derived modes. Respects `doom-modeline-enable-word-count'.
-; ;; If it brings the sluggish issue, disable `doom-modeline-enable-word-count' or
-; ;; remove the modes from `doom-modeline-continuous-word-count-modes'.
-; (setq doom-modeline-continuous-word-count-modes '(markdown-mode gfm-mode org-mode))
-;
-; ;; Whether display the buffer encoding.
-; (setq doom-modeline-buffer-encoding t)
+;;   relative-from-project => emacs/lisp/comint.el
+;;   relative-to-project => lisp/comint.el
+;;   file-name => comint.el
+;;   buffer-name => comint.el<2> (uniquify buffer name)
+;;
+;; If you are experiencing the laggy issue, especially while editing remote files
+;; with tramp, please try `file-name' style.
+;; Please refer to https://github.com/bbatsov/projectile/issues/657.
+(setq doom-modeline-buffer-file-name-style 'auto)
+
+;; Whether display icons in the mode-line.
+;; While using the server mode in GUI, should set the value explicitly.
+(setq doom-modeline-icon (display-graphic-p))
+
+;; Whether display the icon for `major-mode'. It respects `doom-modeline-icon'.
+(setq doom-modeline-major-mode-icon t)
+
+;; Whether display the colorful icon for `major-mode'.
+;; It respects `all-the-icons-color-icons'.
+(setq doom-modeline-major-mode-color-icon t)
+
+;; Whether display the icon for the buffer state. It respects `doom-modeline-icon'.
+(setq doom-modeline-buffer-state-icon t)
+
+;; Whether display the modification icon for the buffer.
+;; It respects `doom-modeline-icon' and `doom-modeline-buffer-state-icon'.
+(setq doom-modeline-buffer-modification-icon t)
+
+;; Whether to use unicode as a fallback (instead of ASCII) when not using icons.
+(setq doom-modeline-unicode-fallback nil)
+
+;; Whether display the minor modes in the mode-line.
+(setq doom-modeline-minor-modes nil)
+
+;; If non-nil, a word count will be added to the selection-info modeline segment.
+(setq doom-modeline-enable-word-count nil)
+
+;; Major modes in which to display word count continuously.
+;; Also applies to any derived modes. Respects `doom-modeline-enable-word-count'.
+;; If it brings the sluggish issue, disable `doom-modeline-enable-word-count' or
+;; remove the modes from `doom-modeline-continuous-word-count-modes'.
+(setq doom-modeline-continuous-word-count-modes '(markdown-mode gfm-mode org-mode))
+
+;; Whether display the buffer encoding.
+(setq doom-modeline-buffer-encoding t)
 
 ;; Whether display the indentation information.
 (setq doom-modeline-indent-info nil)
