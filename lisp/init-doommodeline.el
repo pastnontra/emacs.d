9b66dc6169f50ba48880c7791926e70dc0d2d31c
Pastnontra <pastnontra@gmail.com>
2022-02-25 22:51:05 +0800

    Fix CI

--- lisp/init-doommodeline.el
+++ lisp/init-doommodeline.el
@@ -1,92 +1,92 @@
-;; -*- coding: utf-8; lexical-binding: t; -*-
-
-(require 'doom-modeline)
-(doom-modeline-mode 1)
-
-;; How tall the mode-line should be. It's only respected in GUI.
-;; If the actual char height is larger, it respects the actual height.
-(setq doom-modeline-height 25)
-
-;; How wide the mode-line bar should be. It's only respected in GUI.
-(setq doom-modeline-bar-width 4)
-
-;; Whether to display the bar to indicate active and inactive windows.
-;; It's only respected in GUI.
-(setq doom-modeline-bar t)
-
-;; Whether to use hud bar to indicate the buffer position.
-;; It's only respected in GUI.
-(setq doom-modeline-hud nil)
-
-;; The limit of the window width.
-;; If `window-width' is smaller than the limit, some information won't be displayed.
-(setq doom-modeline-window-width-limit fill-column)
-
-;; How to detect the project root.
-;; The default priority of detection is `ffip' > `projectile' > `project'.
-;; nil means to use `default-directory'.
-;; The project management packages have some issues on detecting project root.
-;; e.g. `projectile' doesn't handle symlink folders well, while `project' is unable
-;; to hanle sub-projects.
-;; You can specify one if you encounter the issue.
-(setq doom-modeline-project-detection 'project)
-
-;; Determines the style used by `doom-modeline-buffer-file-name'.
-;;
-;; Given ~/Projects/FOSS/emacs/lisp/comint.el
-;;   auto => emacs/lisp/comint.el (in a project) or comint.el
-;;   truncate-upto-project => ~/P/F/emacs/lisp/comint.el
-;;   truncate-from-project => ~/Projects/FOSS/emacs/l/comint.el
-;;   truncate-with-project => emacs/l/comint.el
-;;   truncate-except-project => ~/P/F/emacs/l/comint.el
-;;   truncate-upto-root => ~/P/F/e/lisp/comint.el
-;;   truncate-all => ~/P/F/e/l/comint.el
-;;   truncate-nil => ~/Projects/FOSS/emacs/lisp/comint.el
-;;   relative-from-project => emacs/lisp/comint.el
-;;   relative-to-project => lisp/comint.el
-;;   file-name => comint.el
-;;   buffer-name => comint.el<2> (uniquify buffer name)
-;;
-;; If you are experiencing the laggy issue, especially while editing remote files
-;; with tramp, please try `file-name' style.
-;; Please refer to https://github.com/bbatsov/projectile/issues/657.
-(setq doom-modeline-buffer-file-name-style 'auto)
-
-;; Whether display icons in the mode-line.
-;; While using the server mode in GUI, should set the value explicitly.
-(setq doom-modeline-icon (display-graphic-p))
-
-;; Whether display the icon for `major-mode'. It respects `doom-modeline-icon'.
-(setq doom-modeline-major-mode-icon t)
-
-;; Whether display the colorful icon for `major-mode'.
-;; It respects `all-the-icons-color-icons'.
-(setq doom-modeline-major-mode-color-icon t)
-
-;; Whether display the icon for the buffer state. It respects `doom-modeline-icon'.
-(setq doom-modeline-buffer-state-icon t)
-
-;; Whether display the modification icon for the buffer.
-;; It respects `doom-modeline-icon' and `doom-modeline-buffer-state-icon'.
-(setq doom-modeline-buffer-modification-icon t)
-
-;; Whether to use unicode as a fallback (instead of ASCII) when not using icons.
-(setq doom-modeline-unicode-fallback nil)
-
-;; Whether display the minor modes in the mode-line.
-(setq doom-modeline-minor-modes nil)
-
-;; If non-nil, a word count will be added to the selection-info modeline segment.
-(setq doom-modeline-enable-word-count nil)
-
-;; Major modes in which to display word count continuously.
-;; Also applies to any derived modes. Respects `doom-modeline-enable-word-count'.
-;; If it brings the sluggish issue, disable `doom-modeline-enable-word-count' or
-;; remove the modes from `doom-modeline-continuous-word-count-modes'.
-(setq doom-modeline-continuous-word-count-modes '(markdown-mode gfm-mode org-mode))
-
-;; Whether display the buffer encoding.
-(setq doom-modeline-buffer-encoding t)
+; ;; -*- coding: utf-8; lexical-binding: t; -*-
+;
+; (require 'doom-modeline)
+; (doom-modeline-mode 1)
+;
+; ;; How tall the mode-line should be. It's only respected in GUI.
+; ;; If the actual char height is larger, it respects the actual height.
+; (setq doom-modeline-height 25)
+;
+; ;; How wide the mode-line bar should be. It's only respected in GUI.
+; (setq doom-modeline-bar-width 4)
+;
+; ;; Whether to display the bar to indicate active and inactive windows.
+; ;; It's only respected in GUI.
+; (setq doom-modeline-bar t)
+;
+; ;; Whether to use hud bar to indicate the buffer position.
+; ;; It's only respected in GUI.
+; (setq doom-modeline-hud nil)
+;
+; ;; The limit of the window width.
+; ;; If `window-width' is smaller than the limit, some information won't be displayed.
+; (setq doom-modeline-window-width-limit fill-column)
+;
+; ;; How to detect the project root.
+; ;; The default priority of detection is `ffip' > `projectile' > `project'.
+; ;; nil means to use `default-directory'.
+; ;; The project management packages have some issues on detecting project root.
+; ;; e.g. `projectile' doesn't handle symlink folders well, while `project' is unable
+; ;; to hanle sub-projects.
+; ;; You can specify one if you encounter the issue.
+; (setq doom-modeline-project-detection 'project)
+;
+; ;; Determines the style used by `doom-modeline-buffer-file-name'.
+; ;;
+; ;; Given ~/Projects/FOSS/emacs/lisp/comint.el
+; ;;   auto => emacs/lisp/comint.el (in a project) or comint.el
+; ;;   truncate-upto-project => ~/P/F/emacs/lisp/comint.el
+; ;;   truncate-from-project => ~/Projects/FOSS/emacs/l/comint.el
+; ;;   truncate-with-project => emacs/l/comint.el
+; ;;   truncate-except-project => ~/P/F/emacs/l/comint.el
+; ;;   truncate-upto-root => ~/P/F/e/lisp/comint.el
+; ;;   truncate-all => ~/P/F/e/l/comint.el
+; ;;   truncate-nil => ~/Projects/FOSS/emacs/lisp/comint.el
+; ;;   relative-from-project => emacs/lisp/comint.el
+; ;;   relative-to-project => lisp/comint.el
+; ;;   file-name => comint.el
+; ;;   buffer-name => comint.el<2> (uniquify buffer name)
+; ;;
+; ;; If you are experiencing the laggy issue, especially while editing remote files
+; ;; with tramp, please try `file-name' style.
+; ;; Please refer to https://github.com/bbatsov/projectile/issues/657.
+; (setq doom-modeline-buffer-file-name-style 'auto)
+;
+; ;; Whether display icons in the mode-line.
+; ;; While using the server mode in GUI, should set the value explicitly.
+; (setq doom-modeline-icon (display-graphic-p))
+;
+; ;; Whether display the icon for `major-mode'. It respects `doom-modeline-icon'.
+; (setq doom-modeline-major-mode-icon t)
+;
+; ;; Whether display the colorful icon for `major-mode'.
+; ;; It respects `all-the-icons-color-icons'.
+; (setq doom-modeline-major-mode-color-icon t)
+;
+; ;; Whether display the icon for the buffer state. It respects `doom-modeline-icon'.
+; (setq doom-modeline-buffer-state-icon t)
+;
+; ;; Whether display the modification icon for the buffer.
+; ;; It respects `doom-modeline-icon' and `doom-modeline-buffer-state-icon'.
+; (setq doom-modeline-buffer-modification-icon t)
+;
+; ;; Whether to use unicode as a fallback (instead of ASCII) when not using icons.
+; (setq doom-modeline-unicode-fallback nil)
+;
+; ;; Whether display the minor modes in the mode-line.
+; (setq doom-modeline-minor-modes nil)
+;
+; ;; If non-nil, a word count will be added to the selection-info modeline segment.
+; (setq doom-modeline-enable-word-count nil)
+;
+; ;; Major modes in which to display word count continuously.
+; ;; Also applies to any derived modes. Respects `doom-modeline-enable-word-count'.
+; ;; If it brings the sluggish issue, disable `doom-modeline-enable-word-count' or
+; ;; remove the modes from `doom-modeline-continuous-word-count-modes'.
+; (setq doom-modeline-continuous-word-count-modes '(markdown-mode gfm-mode org-mode))
+;
+; ;; Whether display the buffer encoding.
+; (setq doom-modeline-buffer-encoding t)
 
 ;; Whether display the indentation information.
 (setq doom-modeline-indent-info nil)
